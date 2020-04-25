7fbb5ada5a2f595322230c08c51cf899b322d624
Dan Hauer <dlhauer@Dans-MacBook-Pro.local>
2020-04-25 14:14:23 -0700

    removes hardcoded rooms, adds map generation to create_world

--- DungeonAPI/world.py
+++ DungeonAPI/world.py
@@ -4,6 +4,7 @@ import bcrypt
 
 from DungeonAPI.room import Room
 from DungeonAPI.player import Player
+from DungeonAPI.map import Map
 
 from DungeonAPI.models import *
 
@@ -50,36 +51,12 @@ class World:
         if user.password_hash == password_hash:
             return user
         return None
-
+    
     def create_world(self):
-        # UPDATE THIS:
-        # Should create 100 procuedurally generated rooms
-        self.rooms = {
-            'outside':  Room("Outside Cave Entrance",
-                             "North of you, the cave mount beckons", 1, 1, 1),
-
-            'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-        passages run north and east.""", 2, 1, 2),
-
-            'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
-        into the darkness. Ahead to the north, a light flickers in
-        the distance, but there is no way across the chasm.""", 3, 1, 3),
-
-            'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-        to north. The smell of gold permeates the air.""", 4, 2, 2),
-
-            'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
-        chamber! Sadly, it has already been completely emptied by
-        earlier adventurers. The only exit is to the south.""", 5, 2, 3),
-        }
-
-        # Link rooms together
-        self.rooms['outside'].connect_rooms('n', self.rooms['foyer'])
-        self.rooms['foyer'].connect_rooms('n', self.rooms['overlook'])
-        self.rooms['foyer'].connect_rooms('e', self.rooms['narrow'])
-        self.rooms['narrow'].connect_rooms('n', self.rooms['treasure'])
-
-        self.starting_room = self.rooms['outside']
+        map = Map(25, 150)
+        self.rooms = map.generate_rooms()
+        # map.print_grid() 
+        # print(self.rooms)
 
     def save_to_db(self, DB):
         DB.drop_all()
